a9da4c4edc6a278e47c5ef504a943db1667e15fb
Alex.Ramirez <aleramirez201@gmail.com>
2022-01-27 19:29:01 -0600

    idk whats wrong it should work but it doesn't

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 const noteRouter = require('./notes');
 
-app.use('/notes', noteRouter);
+app.use("/notes", noteRouter);
 
 module.exports = app;
