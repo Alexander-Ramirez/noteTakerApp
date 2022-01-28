348c7963f7943fe5114cb65fe06a295bc3a030b0
Alex.Ramirez <aleramirez201@gmail.com>
2022-01-27 21:34:36 -0600

    tried fixing, i believe I'm having issues with uuid

--- routes/notes.js
+++ routes/notes.js
@@ -1,36 +1,37 @@
 const notes = require('express').Router();
-const {v4: uuidv4} = require ('uuid');
+const {uuid} = require ("../helpers/fsUtils");
 const {readFromFile, readAndAppend, writeTofile} = require("../helpers/fsUtils");
 
 notes.get('/', (req, res) => {
-    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+    readFromFile('./db/db.json').then((notes) => res.json(JSON.parse(notes)));
 });
 
 notes.post('/', (req, res) => {
     const {id, title, text} = req.body;
     if (req.body) {
         const newNote = {
-            id: uuidv4(),
+            id: uuid(),
             title,
             text,
         };
 
         readAndAppend(newNote, "./db/db.json");
-        res.json("New note has been added successfully.");
+        res.json("New notes has been added successfully.");
     } else {
-        res.error("Couldn't add new note.");
+        res.error("Couldn't add new notes.");
     };
 });
 
 notes.get('/:id', (req, res) => {
-    const noteID = req.params.id;
-    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+    const noteId = req.params.id;
+    readFromFile('./db/db.json').then((notes) => JSON.parse(notes)).then((json) => {
+        const result = json.filter((note) => note.id === noteId);
+    });
 });
-
 notes.delete('/:id', (req, res) => {
     const noteId = req.params.id;
-    readFromFile('./db/db.json').then((data) => JSON.parse(data)).then((json) => {
-        const result = json.filter((note) => note.id !== noteId);
+    readFromFile('./db/db.json').then((notes) => JSON.parse(notes)).then((json) => {
+        const result = json.filter((notes) => notes.id !== noteId);
         writeTofile("'./db/db.json'", result);
         res.json(`${noteId} has been deleted`)
     })
